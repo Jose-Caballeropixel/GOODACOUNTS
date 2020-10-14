2b80f9a5285479a547d5e9f941ac198d104c0826
dasbeltran99 <dasalazar876@misena.edu.co>
2020-10-14 14:54:33 -0500

    Trabajando Diseño

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -8,9 +8,13 @@
         <div class="opciones-admin">
             <div class="row">
                 <div class="col">
-                <a href=""><img src="{{asset('/img/anadir-evento.png')}}" alt="" width="100">
+                    <div class="icon-enlace">
+                        <a href="" class="">
+                            <img src="/img/anadir-evento.png" alt="" width="100" class="d-block">
+                            <h3>Crear empresa</h3>
+                        </a>
+                    </div>
                 </div>
-                <span>Crear Gerente</span></a>
             </div>
         </div>
     </div>
